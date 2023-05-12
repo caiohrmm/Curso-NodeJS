28948dbe1d6e13470c22011545caa770f34a965c
caiohrmm <cahenromar@gmail.com>
2023-05-12 12:39:10 -0300

    Async e Sync

---
+++ Fundamentos do Node.js/Sync e Async/index.js
@@ -0,0 +1,8 @@
+/* 
+
+Existem duas formas do Node ler meu código -> Assincrona e Sincrona.
+A sincrona e como eu ja estava vendo, ele espera o codigo de cima executar para executar o debaixo
+A assincrona nao depende disso.
+
+*/
+
