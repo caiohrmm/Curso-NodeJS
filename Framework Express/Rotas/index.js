066859e56220df5cf0382b00a533c750d2917b25
Caio Henrique <cahenromar@gmail.com>
2023-05-16 18:24:41 -0300

    Rotas

---
+++ Framework Express/Rotas/index.js
@@ -0,0 +1,6 @@
+// O que sao rotas ? Rotas são uma ponte que ligam o usuário até a lógica da minha aplicacao
+
+// Ex -> /nomes -> é uma rota de nomes
+
+// https://escolabr.com.br/nomes -> Mostrará todos os nomes da minha escola.
+
