1e9eadf9ebbea8330cba0639b9755135e6ffc81f
caiohrmm <cahenromar@gmail.com>
2023-05-19 22:04:39 -0300

    Lendo dados com sequelize

---
+++ ORM Sequelize/Lendo dados com sequelize/db/connection.js
@@ -0,0 +1,13 @@
+// Conectando o sequelize
+// Obs -> O sequelize requer o pacote mysql2
+const { Sequelize } = require("sequelize");
+
+// Agora estanciarei a classe sequelize e passarei minhas informacoes basicas
+/* new Sequelize('database', 'user', 'password', { host: 'localhost', dialect: 'bancoquequerointegrar'}) */
+const sequelize = new Sequelize('sequelize', 'caioh', 'caiohenrique@12345', {
+  host: 'localhost',
+  dialect: 'mysql',
+})
+// Os parametros sao host, user, password e o banco que irei ligar.
+module.exports = sequelize
+
