419a1986f017e4794d6c988bec443d05262bb119
Caio Henrique <cahenromar@gmail.com>
2023-05-20 01:17:00 -0300

    Resgatando dados relacionados

---
+++ ORM Sequelize/Resgatar dados relacionados/index.js
@@ -0,0 +1,146 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+// Conexao com o MySQL através do pool
+const dbconnection = require("./db/connection");
+
+// Importo meus models
+const User = require("./models/User");
+const Address = require("./models/Address");
+
+const port = 3000;
+
+const app = express();
+
+// Setup handlebars
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+// Ler o body para pegar os valores do formulário
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/users/add", (req, res) => {
+  res.render("adduser");
+});
+
+app.get("/users", async (req, res) => {
+  // Terei minha função async pois ela esperará a chegada dos meus dados para renderizar!
+  const users = await User.findAll({ raw: true });
+  // Essa variavel receberá um array de objetos que virá do meu banco.
+  // O parametro raw faz com que os dados já cheguem prontos para meu uso
+  res.render("users", { users });
+});
+
+app.post("/users/insert", async (req, res) => {
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  let isWorking = req.body.isworking;
+  // Vai ser let pois irei mudar seu valor para se adaptar ao booleano.
+
+  if (isWorking === "yes") {
+    isWorking = true;
+  } else {
+    isWorking = false;
+  }
+
+  const data = { name, occupation, isworking: isWorking };
+
+  // Aqui farei o INSERT dos dados pelo sequelize
+  // Preciso de um await pois assim, eu espero o sequelize inserir meus dados para dar continuidade ao meu fluxo.
+  await User.create(data);
+  // Como é desestruturação, ou eu mando o nome da variavel do mesmo nome que a coluna do banco ou faço a troca por : na funcao.
+
+  res.redirect("/users/add");
+});
+
+// Pegando apenas um usuário com o where metodo findOne
+app.get("/users/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const user = await User.findOne({ raw: true, where: { id } });
+
+  res.render("user", { user });
+});
+
+// Fazendo o insert de dados relacionados
+
+app.post("/address/add", async (req, res) => {
+  const id = req.body.id;
+  const street = req.body.street;
+  const number = req.body.number;
+  const city = req.body.city;
+
+  const addressData = { UserId: id, street, number, city };
+
+  await Address.create(addressData);
+
+  res.redirect("/users");
+});
+
+// Excluindo com sequelize
+app.post("/users/delete/:id", async (req, res) => {
+  const id = req.params.id;
+
+  // Aqui, como somente irei excluir, nao preciso de variaveis, somente do comando
+
+  await User.destroy({ where: { id } });
+
+  res.redirect("/users");
+});
+
+app.get("/users/edit/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const user = await User.findOne({ include: Address, where: { id } });
+    // Para pegar dados relacionados juntamente com os dados do usuario, inves do raw
+    // Usarei o include, que facilitará meu uso
+    // Depois, na hora de passar os dados farei um get plain.
+
+    res.render("edituser", { user: user.get({ plain: true }) });
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.post("/users/editing", async (req, res) => {
+  const id = req.body.id;
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  let isWorking = req.body.isworking;
+  if (isWorking === "yes") {
+    isWorking = true;
+  } else {
+    isWorking = false;
+  }
+  const userData = { id, name, occupation, isworking: isWorking };
+
+  await User.update(userData, { where: { id } });
+
+  res.redirect("/users");
+});
+// Agora, pra eu rodar o meu servidor eu vou depender do OK do banco de dados
+dbconnection
+  /* Parametro force -> Esse cara excluir a tabela e todos os dados e a recria novamente */
+  .sync(/*{force:true}*/)
+  .then(
+    app.listen(port, () =>
+      console.log(
+        `Servidor rodando na porta ${port} e conectado ao banco de dados.`
+      )
+    )
+  )
+  .catch((err) => console.log(err));
+
+// O que acontece aqui, ele mapeará meu model para que, ao iniciar o servidor, esteja criada as tabelas e tudo funcionando perfeitamente para a persistencia de dados
+// Se a tabela já existir ele ignora, se existir ele verifica se está tudo OK e abre meu servidor.
