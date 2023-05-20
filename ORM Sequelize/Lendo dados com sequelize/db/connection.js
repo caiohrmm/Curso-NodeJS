efb4f4fe617e12280c7e6a11dd270a7c40d269df
Caio Henrique <cahenromar@gmail.com>
2023-05-19 22:09:16 -0300

    Mudando o host do banco

--- ORM Sequelize/Lendo dados com sequelize/db/connection.js
+++ ORM Sequelize/Lendo dados com sequelize/db/connection.js
@@ -5,7 +5,7 @@ const { Sequelize } = require("sequelize");
 // Agora estanciarei a classe sequelize e passarei minhas informacoes basicas
 /* new Sequelize('database', 'user', 'password', { host: 'localhost', dialect: 'bancoquequerointegrar'}) */
 const sequelize = new Sequelize('sequelize', 'caioh', 'caiohenrique@12345', {
-  host: 'localhost',
+  host: '26.185.31.219',
   dialect: 'mysql',
 })
 // Os parametros sao host, user, password e o banco que irei ligar.
