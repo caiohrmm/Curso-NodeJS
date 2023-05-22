7b3d52038bad50b36d4ec047fa14987eae4f436e
caiohrmm <cahenromar@gmail.com>
2023-05-22 10:49:49 -0300

    Estrutura MVC

---
+++ Modelo MVC/Introducao MVC/index.js
@@ -0,0 +1,14 @@
+/* Minha estrutura de projetos utilizando o MVC */
+
+/*
+
+Pastas -> 
+
+controllers -> Ficarao todos os arquivos de controle da minha aplicação
+views -> Ficarao todos os arquivos de view 
+models -> Ficarao todos os models
+routes -> Todas as rotas da minha aplicacao
+index.js -> Aonde vai rodar minha aplicacao
+
+
+*/
