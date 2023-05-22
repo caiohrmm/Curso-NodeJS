42ca4e5ae30486fc4fd73db635a502bded79fa07
caiohrmm <cahenromar@gmail.com>
2023-05-22 13:09:44 -0300

    Salvando dados no banco - Método create

---
+++ Modelo MVC/Salvando dados no banco/routes/tasksRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+// Em cada rota irei utilizar uma função do controller
+// Importando o controller responsável pela rota ou conjunto de rotas.
+const TaskController = require("../controllers/TaskController");
+
+router.get("/", TaskController.viewAllTasks);
+router.get("/add", TaskController.createTask);
+router.post("/save", TaskController.saveTask);
+
+module.exports = router;
