3e3e11dcf536da22f3d8fce6f57c5ec45fcb74b2
caiohrmm <cahenromar@gmail.com>
2023-05-22 12:10:47 -0300

    Criando rotas

---
+++ Modelo MVC/Criando as rotas/routes/tasksRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// Em cada rota irei utilizar uma função do controller
+// Importando o controller responsável pela rota ou conjunto de rotas.
+const TaskController = require("../controllers/TaskController");
+
+router.get("/", TaskController.viewAllTasks);
+router.get("/add", TaskController.createTask);
+
+module.exports = router;
