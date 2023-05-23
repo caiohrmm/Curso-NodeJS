a8766959a681050d8670e21d338fd2a69d23bb9c
caiohrmm <cahenromar@gmail.com>
2023-05-23 16:04:17 -0300

    Requirindo pacotes

--- Projeto Thoughts/index.js
+++ Projeto Thoughts/index.js
@@ -1,1 +1,10 @@
+// Requirindo pacotes necessários
 const express = require('express')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const FileStore = require('session-file-store')
+const flash = require('express-flash')
+
+// Instanciando express
+const app = express()
+
