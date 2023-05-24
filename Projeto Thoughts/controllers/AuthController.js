54b183c373cda9c538c6f9c4f9c24d56ebedfbbc
caiohrmm <cahenromar@gmail.com>
2023-05-24 01:13:13 -0300

    Implementando flash message de senha diferente

--- Projeto Thoughts/controllers/AuthController.js
+++ Projeto Thoughts/controllers/AuthController.js
@@ -1,9 +1,29 @@
+const User = require('../models/User') // Chamo o usuario para salva-lo no banco
+
+const bcrypt = require('bcryptjs') // Para mandar as senhas ja criptografadas para o banco
+
 module.exports = class AuthController {
     static login(req, res) {
         res.render('auth/login')
     }
     static register(req, res) {
         res.render('auth/register')
+    }
+    static async registerPost(req,res) {
+        // Primeiro as validacoes
+        const {username, email, password, confirmpassword} = req.body
+
+        // Verificar se a senha colocada é igual a confirmada!
+
+        if (password != confirmpassword) {
+            // Enviar mensagem de erro para meu front
+            req.flash('message', 'As senhas não são iguais. Tente novamente!')
+
+            res.render('auth/register')
+
+            return
+        }
+
     }
 
 }
