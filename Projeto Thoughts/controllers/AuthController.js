b9a09f84f6a26e7081ded5e1ef154a4c40706a31
caiohrmm <cahenromar@gmail.com>
2023-05-24 01:49:20 -0300

    Criando um usuário e autenticando ele em uma sessao

--- Projeto Thoughts/controllers/AuthController.js
+++ Projeto Thoughts/controllers/AuthController.js
@@ -1,29 +1,63 @@
-const User = require('../models/User') // Chamo o usuario para salva-lo no banco
+const User = require("../models/User"); // Chamo o usuario para salva-lo no banco
 
-const bcrypt = require('bcryptjs') // Para mandar as senhas ja criptografadas para o banco
+const bcrypt = require("bcryptjs"); // Para mandar as senhas ja criptografadas para o banco
 
 module.exports = class AuthController {
-    static login(req, res) {
-        res.render('auth/login')
-    }
-    static register(req, res) {
-        res.render('auth/register')
-    }
-    static async registerPost(req,res) {
-        // Primeiro as validacoes
-        const {username, email, password, confirmpassword} = req.body
+  static login(req, res) {
+    res.render("auth/login");
+  }
+  static register(req, res) {
+    res.render("auth/register");
+  }
+  static async registerPost(req, res) {
+    // Primeiro as validacoes
+    const { username, email, password, confirmpassword } = req.body;
 
-        // Verificar se a senha colocada é igual a confirmada!
+    // Verificar se a senha colocada é igual a confirmada!
+    if (password != confirmpassword) {
+      // Enviar mensagem de erro para meu front
+      req.flash("message", "As senhas não são iguais. Tente novamente!");
 
-        if (password != confirmpassword) {
-            // Enviar mensagem de erro para meu front
-            req.flash('message', 'As senhas não são iguais. Tente novamente!')
+      res.render("auth/register");
 
-            res.render('auth/register')
-
-            return
-        }
-
+      return;
     }
 
-}
+    // Verificar se o email que será registrado já existe no banco
+    const checkEmail = await User.findOne({ where: { email } });
+
+    if (checkEmail) {
+      // Se tiver algo dentro desse checkEmail ele cai aqui no if
+      req.flash("message", "O e-mail já está em uso. Tente novamente!");
+
+      res.render("auth/register");
+
+      return;
+    }
+
+    // Criar minha senha criptografada
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const user = {
+      name: username,
+      email,
+      password: hashedPassword,
+    };
+    try {
+      const createUser = await User.create(user);
+
+      // Agora preciso autenticá-lo na minha sessao
+      req.session.userId = createUser.id;
+
+      req.flash("messagesuccess", "Usuário cadastrado com sucesso!");
+
+      req.session.save(() => {
+        // Com isso, eu consigo salvar minha sessao e ver se o usuario está autenticado ou nao no meu frontend
+        res.redirect("/");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
