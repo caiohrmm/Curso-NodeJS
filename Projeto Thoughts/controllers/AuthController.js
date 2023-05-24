b0f783b045ffd26d9de7b510ee59f7fc080646d1
caiohrmm <cahenromar@gmail.com>
2023-05-24 11:23:45 -0300

    Autenticacao de usuario concluida

--- Projeto Thoughts/controllers/AuthController.js
+++ Projeto Thoughts/controllers/AuthController.js
@@ -65,4 +65,49 @@ module.exports = class AuthController {
     req.session.destroy();
     res.redirect("/login");
   }
+
+  static async loginPost(req, res) {
+    // Funcao que faz a autenticacao do usuario no login
+    const { email, password } = req.body;
+
+    // Verificar se o usuario existe
+    const user = await User.findOne({ where: { email }, raw: true });
+
+    if (!user) {
+      req.flash(
+        "message",
+        "Esse e-mail não existe em nosso sistema. Tente novamente!"
+      );
+
+      res.render("auth/login");
+
+      return;
+    }
+
+    // Verificar se a senha consta com a registrada no sistema
+    const passwordMatch = bcrypt.compareSync(password, user.password); // Retorna true ou false
+
+    if (!passwordMatch) {
+      req.flash("message", "Senha incorreta. Tente novamente!");
+
+      res.render("auth/login");
+
+      return;
+    }
+
+    // Se ele passou por todas as validacoes, agora eu logo ele!
+
+    // Agora preciso autenticá-lo na minha sessao
+    req.session.userId = user.id;
+
+    req.flash("messagesuccess", "Login realizado com sucesso!");
+
+    req.session.save(() => {
+      // Com isso, eu consigo salvar minha sessao e ver se o usuario está autenticado ou nao no meu frontend
+      res.redirect("/");
+    });
+  }
+  catch(error) {
+    console.log(error);
+  }
 };
