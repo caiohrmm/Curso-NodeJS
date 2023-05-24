a6b9211ff9ddbd87415f2ade348152ae64ce74fd
caiohrmm <cahenromar@gmail.com>
2023-05-24 11:35:20 -0300

    Criacao do middleware que protege as rotas

--- Projeto Thoughts/routes/thoughtsRoutes.js
+++ Projeto Thoughts/routes/thoughtsRoutes.js
@@ -5,6 +5,10 @@ const router = express.Router()
 // Importando meu controller
 const ThoughtController = require('../controllers/ThoughtController')
 
+// Importando meu helper
+const checkAuth = require('../helpers/auth').checkAuth
+
+router.get('/dashboard', checkAuth, ThoughtController.dashboard) // O middleware será executado sempre que a rota for acessada.
 router.get('/', ThoughtController.showThoughts)
 
 module.exports = router
