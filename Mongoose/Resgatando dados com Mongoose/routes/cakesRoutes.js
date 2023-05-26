b090b667739987a6e52e5e3262ee0d1e18d8d118
caiohrmm <cahenromar@gmail.com>
2023-05-26 20:18:43 -0300

    Resgatando dados com Mongoose

---
+++ Mongoose/Resgatando dados com Mongoose/routes/cakesRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const CakeController = require("../controllers/CakeController");
+
+router.get('/', CakeController.showAllCakes)
+router.get('/create', CakeController.createCake)
+router.post('/create', CakeController.createCakeSave)
+router.get('/:id', CakeController.getOneCake)
+router.post('/delete/:id', CakeController.deleteOneCake)
+
+module.exports = router;
