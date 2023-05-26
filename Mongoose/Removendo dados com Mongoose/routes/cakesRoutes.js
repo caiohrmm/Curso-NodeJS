0bbc985ed2f3691361942c6d96c6ace9b4719851
caiohrmm <cahenromar@gmail.com>
2023-05-26 20:43:55 -0300

    Removendo os dados com o Mongoose

---
+++ Mongoose/Removendo dados com Mongoose/routes/cakesRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const CakeController = require("../controllers/CakeController");
+
+router.get('/', CakeController.showAllCakes)
+router.get('/create', CakeController.createCake)
+router.post('/create', CakeController.createCakeSave)
+router.get('/:id', CakeController.getOneCake)
+router.post('/delete/:id', CakeController.deleteOneCake)
+router.get('/edit/:id', CakeController.viewEditCake)
+router.post('/edit', CakeController.editCake)
+
+
+module.exports = router;
