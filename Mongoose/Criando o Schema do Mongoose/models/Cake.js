eb24cd3f24b560dee09f216ec21e30c4c2f266e4
caiohrmm <cahenromar@gmail.com>
2023-05-26 20:08:11 -0300

    Criando schema

--- Mongoose/Criando o Schema do Mongoose/models/Cake.js
+++ Mongoose/Criando o Schema do Mongoose/models/Cake.js
@@ -1,51 +1,16 @@
-const connection = require("../db/connection");
+// Criando meu Schema
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const { ObjectId } = require("mongodb");
-
-class Cake {
-  // Na classe eu irei construir os atributos da minha collection
-  constructor(name, image, price, description) {
-    this.name = name;
-    (this.image = image), (this.price = price);
-    this.description = description;
-  }
-
-  // Função que salva os dados no banco, faz o insert.
-  save() {
-    // Esse save será responsavel por criar a collection e inserir os campos de name, price e description nela.
-    const cake = connection.db().collection("cakes").insertOne({
-      name: this.name,
-      price: this.price,
-      image: this.image,
-      description: this.description,
-    });
-    return cake;
-  }
-
-  // Função que pega os dados do banco, faz um find() -> SELECT
-
-  static async getAllCakes() {
-    const cakes = await connection.db().collection("cakes").find().toArray();
-    // Gero um array com todos meus documents(dados)
-    return cakes;
-  }
-
-  static async getCakeById(id) {
-    const objectId = new ObjectId(id);
-    const cake = await connection
-      .db()
-      .collection("cakes")
-      .findOne({ _id: objectId });
-    // Uso o metodo ObjectId importado do mongodb para converter o numero no id do objeto requerido.
-    return cake;
-  }
-
-  static async deleteCakeById(id) {
-    const objectId = new ObjectId(id);
-    await connection.db().collection("cakes").deleteOne({ _id: objectId });
-
-    return;
-  }
-}
+// Criando meu Schema, preciso do nome do model e das propriedades da minha collection que será criada.
+const Cake = mongoose.model(
+  "Cake",
+  new Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+  })
+);
 
 module.exports = Cake;
