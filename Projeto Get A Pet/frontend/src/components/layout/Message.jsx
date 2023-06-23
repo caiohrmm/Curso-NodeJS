75cae7735c7436481552d105c32a651b5565245d
caiohrmm <cahenromar@gmail.com>
2023-06-23 17:04:42 -0300

    Finalizacao do componente de flash message

--- Projeto Get A Pet/frontend/src/components/layout/Message.jsx
+++ Projeto Get A Pet/frontend/src/components/layout/Message.jsx
@@ -1,18 +1,34 @@
-import styles from './Message.module.css'
+import styles from "./Message.module.css";
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
+
+import bus from "../../utils/bus";
 
 const Message = () => {
-    const [type, setType] = useState('')
+  // Estado que manipula se a mensagem é de sucesso ou de erro
+  const [type, setType] = useState("");
 
-    const handleClick = () => {
-        setType('error')
-    }
+  // Estado que manipula se a flash message vai estar visivel ou nao
+  const [visibility, setVisibility] = useState(false);
 
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    bus.addListener('flash', ({ message, type }) => {
+      setVisibility(true);
+      setMessage(message);
+      setType(type);
+      setTimeout(() => {
+        setVisibility(false);
+      }, 10000);
+    });
+  }, []);
 
   return (
-    <div className={`${styles.message} ${styles[type]}`} onClick={handleClick}>Mensagem</div>
-  )
-}
+    visibility && (
+      <div className={`${styles.message} ${styles[type]}`}>{message} </div>
+    )
+  );
+};
 
-export default Message
+export default Message;
--- Projeto Get A Pet/frontend/src/hooks/useAuth.js
+++ Projeto Get A Pet/frontend/src/hooks/useAuth.js
@@ -5,17 +5,27 @@ import api from "../utils/api";
 
 import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import useFlashMessage from "./useFlashMessage";
 
 export default function useAuth() {
+  const { setFlashMessage } = useFlashMessage();
+
   async function register(user) {
+    let msgText = "Cadastro realizado com sucesso!";
+    let msgType = "success";
+
     try {
       // Aqui eu farei basicamente o que eu estava fazendo com o postman, mando dados para minha API e ela me retorna algo.
       const data = await api
         .post("/users/register", user)
         .then((response) => response.data);
     } catch (error) {
-      console.log(error);
+      // Caso de algum erro, ele aparece na tela a mensagem de erro de validacao da API
+      msgText = error.response.data.message;
+      msgType = "error";
     }
+
+    setFlashMessage(msgText, msgType)
   }
   return { register };
 }
