69da5810b5b61533dcd2d454a9374bf44d7cde88
caiohrmm <cahenromar@gmail.com>
2023-06-26 17:43:24 -0300

    Criando funcao para editar usuario

--- Projeto Get A Pet/frontend/src/components/pages/User/Profile.jsx
+++ Projeto Get A Pet/frontend/src/components/pages/User/Profile.jsx
@@ -5,42 +5,81 @@ import styles from "./Profile.module.css";
 // Importando API
 import api from "../../../utils/api";
 
+// Importando flash messages
+import useFlashMessage from "../../../hooks/useFlashMessage";
+
 // Importando componentes
 import Input from "../../form/Input";
 import { useEffect, useState } from "react";
 
 const Profile = () => {
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({});
   // Pego o token do meu localStorage
   const [token] = useState(localStorage.getItem("token" || ""));
 
+  const { setFlashMessage } = useFlashMessage();
   useEffect(() => {
-    api.get('/users/checkuser', {
-      headers: {
-        Authorization: `Bearer ${JSON.parse(token)}`
-      }
-    }).then((response) => setUser(response.data))
+    api
+      .get("/users/checkuser", {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then((response) => setUser(response.data));
   }, [token]);
 
+  const onFileChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.files[0] });
+  };
 
-  const onFileChange = (e) => {};
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+    // Pega todos os dados do formulario e salva dentro do objeto user.
+  };
 
-  const handleChange = (e) => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  
+    let msgType = "success";
+
+    const formData = new FormData();
+
+    // Me retorna um array com os dados do meu usuário
+    await Object.keys(user).forEach((key) => {
+      formData.append(key, user[key]);
+    });
+
+    const data = await api
+      .patch(`/users/edit/${user._id}`, formData, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        return response.data;
+      })
+      .catch((err) => {
+        msgType = "error";
+        return err.response.data;
+      });
+
+    setFlashMessage(data.message, msgType);
+  };
+
   return (
     <section>
       <div className={styles.profile_container}>
         <h1>Perfil</h1>
         <p>Preview da imagem</p>
+      </div>
+      <form className={formStyles.form_container} onSubmit={handleSubmit}>
         <Input
           text="Selecione uma imagem"
           type="file"
           name="image"
           handleOnChange={onFileChange}
         />
-      </div>
-      <form className={formStyles.form_container}>
         <Input
           text="Nome"
           type="text"
@@ -78,7 +117,7 @@ const Profile = () => {
         <Input
           text="Confirme sua senha"
           type="password"
-          name="password"
+          name="confirmpassword"
           handleOnChange={handleChange}
           placeholder="Confirme sua senha..."
         />
