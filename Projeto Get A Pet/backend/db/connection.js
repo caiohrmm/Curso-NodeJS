5be27db09b000ae38d450c098f541bdcdf394722
caiohrmm <cahenromar@gmail.com>
2023-07-01 00:06:49 -0300

    Conectando mongoose com container docker

--- Projeto Get A Pet/backend/db/connection.js
+++ Projeto Get A Pet/backend/db/connection.js
@@ -3,9 +3,7 @@
 const mongoose = require("mongoose");
 
 async function main() {
-  await mongoose.connect(
-    "mongodb://127.0.0.1:27017/getapet?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.9.1",
-  );
+  await mongoose.connect("mongodb://127.0.0.1:27017/getapet");
 
   console.log("Conectou ao Mongoose!");
 }
