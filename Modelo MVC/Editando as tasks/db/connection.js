1d1337eeddaebcf90581419510bf9d1f52f51065
caiohrmm <cahenromar@gmail.com>
2023-06-30 23:00:31 -0300

    Mudando para docker

--- Modelo MVC/Editando as tasks/db/connection.js
+++ Modelo MVC/Editando as tasks/db/connection.js
@@ -1,8 +1,8 @@
 // Fazendo a conexao com o banco pelo sequelize
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("estruturamvc", "caioh", "caiohenrique@12345", {
-  host: "26.185.31.219",
+const sequelize = new Sequelize("taskslist", "root", "secreto", {
+  host: "localhost",
   dialect: "mysql",
 });
 
