7e6d3ce5ffb3dfccd17fc08fa5cdd0917c0b0bdc
caiohrmm <cahenromar@gmail.com>
2023-07-04 19:29:32 -0300

    Mudança em estilização do footer

--- Projeto Get A Pet/frontend/src/components/layout/Footer.jsx
+++ Projeto Get A Pet/frontend/src/components/layout/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <p>
-        <span className="bold">Get a Pet by Caio Henrique &copy; 2023</span> 
+        <span className={styles.title}>Get a Pet by Caio Henrique &copy; 2023</span> 
         <span className={styles.desc}>Meu primeiro projeto desenvolvido com API's e ReactJS.</span>
       </p>
     </footer>
