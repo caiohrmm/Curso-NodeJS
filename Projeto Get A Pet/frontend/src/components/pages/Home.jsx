3eb4fd1b3b07599414776b6b4ba30449ecfb4865
Caio Henrique <cahenromar@gmail.com>
2023-07-05 01:28:39 -0300

    Criando minha home

--- Projeto Get A Pet/frontend/src/components/pages/Home.jsx
+++ Projeto Get A Pet/frontend/src/components/pages/Home.jsx
@@ -1,9 +1,35 @@
+import api from "../../utils/api";
+
+import { useState, useEffect } from "react";
+
+import NavLink from "react-router-dom";
+
+import styles from "./Home.module.css";
+
 const Home = () => {
+  // State de pets
+  const [pets, setPets] = useState([]);
+
+  // Requerir todos os pets da minha api
+  useEffect(() => {
+    api.get("/pets").then((response) => {
+      setPets(response.data.pets);
+    });
+  }, []);
+
   return (
     <section>
-        <h1>Home</h1>
+      <div className={styles.header}>
+        <h1>Adote um Pet !</h1>
+        <p>Veja todos os detalhes dos Pets e conheça seus tutores.</p>
+      </div>
+      <div className={styles.container}>
+        {pets.length > 0 &&
+          pets.map((pet) => <div className={styles.pet}></div>)}
+        {pets.length === 0 && <p>Não existem pets cadastrados no sistema!</p>}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
